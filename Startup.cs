5e7dce05e9157bb3f74a8cd25f523708264137ae
Robert Hardy <helloarchy@gmail.com>
2019-12-20 22:22:44 +0000

    Exploring forms, with nested attributes and more.

--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,7 @@ namespace Workings
         {
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/dist"; });
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "WorkingsApp/dist"; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,7 +60,7 @@ namespace Workings
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "WorkingsApp";
 
                 if (env.IsDevelopment())
                 {
